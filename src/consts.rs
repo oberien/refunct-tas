3546cdf3d000e80cf5dc549d20c4fd9ade7fcce4
oberien <jaro.fietz@gmx.de>
2017-01-21 23:39:27 +0100

    doc(consts): Add method names and Linux debug symbol names to pointers

--- src/consts.rs
+++ src/consts.rs
@@ -1,7 +1,21 @@
+// FEngineLoop::Tick(FEngineLoop *__hidden this)
+// _ZN11FEngineLoop4TickEv
 pub const FENGINELOOP_TICK_AFTER_UPDATETIME: usize = 0x169DC95;
+// FApp::DeltaTime
+// static variable inside the binary
 pub const APP_DELTATIME: usize = 0x51CB4A0;
+// FSlateApplication::Tick(FSlateApplication *__hidden this)
+// _ZN17FSlateApplication4TickEv
 pub const FSLATEAPPLICATION_TICK: usize = 0x1A6D0B0;
+// AMyCharacter::ForcedUnCrouch(AMyCharacter *__hidden this)
+// _ZN12AMyCharacter14ForcedUnCrouchEv
 pub const AMYCHARACTER_EXECFORCEDUNCROUCH: &'static str = "_ZN12AMyCharacter18execForcedUnCrouchER6FFramePv";
+// FSlateApplication::OnKeyDown(FSlateApplication *this, unsigned int, unsigned int, bool)
+// _ZN17FSlateApplication9OnKeyDownEijb
 pub const FSLATEAPPLICATION_ONKEYDOWN: &'static str = "_ZN17FSlateApplication9OnKeyDownEijb";
+// FSlateApplication::OnKeyUp(FSlateApplication *this, unsigned int, unsigned int, bool)
+// _ZN17FSlateApplication7OnKeyUpEijb
 pub const FSLATEAPPLICATION_ONKEYUP: &'static str = "_ZN17FSlateApplication7OnKeyUpEijb";
+// FSlateApplication::OnRawMouseMove(FSlateApplication *this, int, int)
+// _ZN17FSlateApplication14OnRawMouseMoveEii
 pub const FSLATEAPPLICATION_ONRAWMOUSEMOVE: &'static str = "_ZN17FSlateApplication14OnRawMouseMoveEii";
