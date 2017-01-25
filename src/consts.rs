3b0bb8df9ddcc422b84691f1655b3ca835a70dbb
oberien <jaro.fietz@gmx.de>
2017-01-25 18:34:49 +0100

    feat(consts): Update pointers to new Linux build

--- src/consts.rs
+++ src/consts.rs
@@ -15,30 +15,30 @@ pub const APP_DELTATIME: usize = OFFSET + 0x2239A68;
 // FSlateApplication::Tick(FSlateApplication *__hidden this)
 // _ZN17FSlateApplication4TickEv
 #[cfg(unix)]
-pub const FSLATEAPPLICATION_TICK: usize = 0x1cd13f0;
+pub const FSLATEAPPLICATION_TICK: usize = 0x1CD1390;
 #[cfg(windows)]
 pub const FSLATEAPPLICATION_TICK: usize = OFFSET + 0x730be0;
 // AMyCharacter::ForcedUnCrouch(AMyCharacter *__hidden this)
 // _ZN12AMyCharacter14ForcedUnCrouchEv
 #[cfg(unix)]
-pub const AMYCHARACTER_EXECFORCEDUNCROUCH: usize = 0x192b0a0;
+pub const AMYCHARACTER_EXECFORCEDUNCROUCH: usize = 0x192b040;
 #[cfg(windows)]
 pub const AMYCHARACTER_EXECFORCEDUNCROUCH: usize = OFFSET + 0x5007c0;
 // FSlateApplication::OnKeyDown(FSlateApplication *this, unsigned int, unsigned int, bool)
 // _ZN17FSlateApplication9OnKeyDownEijb
 #[cfg(unix)]
-pub const FSLATEAPPLICATION_ONKEYDOWN: usize = 0x1ce0b10;
+pub const FSLATEAPPLICATION_ONKEYDOWN: usize = 0x1ce0ab0;
 #[cfg(windows)]
 pub const FSLATEAPPLICATION_ONKEYDOWN: usize = OFFSET + 0x721750;
 // FSlateApplication::OnKeyUp(FSlateApplication *this, unsigned int, unsigned int, bool)
 // _ZN17FSlateApplication7OnKeyUpEijb
 #[cfg(unix)]
-pub const FSLATEAPPLICATION_ONKEYUP: usize = 0x1ce1c40;
+pub const FSLATEAPPLICATION_ONKEYUP: usize = 0x1ce1be0;
 #[cfg(windows)]
 pub const FSLATEAPPLICATION_ONKEYUP: usize = OFFSET + 0x7218f0;
 // FSlateApplication::OnRawMouseMove(FSlateApplication *this, int, int)
 // _ZN17FSlateApplication14OnRawMouseMoveEii
 #[cfg(unix)]
-pub const FSLATEAPPLICATION_ONRAWMOUSEMOVE: usize = 0x1ce9cd0;
+pub const FSLATEAPPLICATION_ONRAWMOUSEMOVE: usize = 0x1ce9c70;
 #[cfg(windows)]
 pub const FSLATEAPPLICATION_ONRAWMOUSEMOVE: usize = OFFSET + 0x722610;
