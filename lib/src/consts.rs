479b6ef7f3051045d81771f3810833feb62a6f8b
oberien <jaro.fietz@gmx.de>
2017-07-05 12:54:06 +0200

    Update windows function pointers

--- lib/src/consts.rs
+++ lib/src/consts.rs
@@ -3,7 +3,7 @@
 #[cfg(unix)]
 pub const FENGINELOOP_TICK_AFTER_UPDATETIME: usize = 0x1905D98;
 #[cfg(windows)]
-pub const FENGINELOOP_TICK_AFTER_UPDATETIME: usize = 0xe8bcc;
+pub const FENGINELOOP_TICK_AFTER_UPDATETIME: usize =  0xe8e67;
 
 #[cfg(unix)]
 pub const GMALLOC: usize = 0x58d8e20;
@@ -13,38 +13,38 @@ pub const GMALLOC: usize = 0x58d8e20;
 #[cfg(unix)]
 pub const APP_DELTATIME: usize = 0x57F0F40;
 #[cfg(windows)]
-pub const APP_DELTATIME: usize = 0x1e39a68;
+pub const APP_DELTATIME: usize = 0x1e1eb08;
 // FSlateApplication::Tick(FSlateApplication *__hidden this)
 // _ZN17FSlateApplication4TickEv
 #[cfg(unix)]
 pub const FSLATEAPPLICATION_TICK: usize = 0x1cd1390;
 #[cfg(windows)]
-pub const FSLATEAPPLICATION_TICK: usize = 0x330be0;
+pub const FSLATEAPPLICATION_TICK: usize = 0x338d60;
 // AMyCharacter::ForcedUnCrouch(AMyCharacter *__hidden this)
 // _ZN12AMyCharacter14ForcedUnCrouchEv
 #[cfg(unix)]
 pub const AMYCHARACTER_EXECFORCEDUNCROUCH: usize = 0x192b040;
 #[cfg(windows)]
 // on Windows we hook before the return
-pub const AMYCHARACTER_EXECFORCEDUNCROUCH: usize = 0x1007d9;
+pub const AMYCHARACTER_EXECFORCEDUNCROUCH: usize = 0x1092f0;
 // FSlateApplication::OnKeyDown(FSlateApplication *this, unsigned int, unsigned int, bool)
 // _ZN17FSlateApplication9OnKeyDownEijb
 #[cfg(unix)]
 pub const FSLATEAPPLICATION_ONKEYDOWN: usize = 0x1ce0ab0;
 #[cfg(windows)]
-pub const FSLATEAPPLICATION_ONKEYDOWN: usize = 0x321750;
+pub const FSLATEAPPLICATION_ONKEYDOWN: usize = 0x329680;
 // FSlateApplication::OnKeyUp(FSlateApplication *this, unsigned int, unsigned int, bool)
 // _ZN17FSlateApplication7OnKeyUpEijb
 #[cfg(unix)]
 pub const FSLATEAPPLICATION_ONKEYUP: usize = 0x1ce1be0;
 #[cfg(windows)]
-pub const FSLATEAPPLICATION_ONKEYUP: usize = 0x3218f0;
+pub const FSLATEAPPLICATION_ONKEYUP: usize = 0x329820;
 // FSlateApplication::OnRawMouseMove(FSlateApplication *this, int, int)
 // _ZN17FSlateApplication14OnRawMouseMoveEii
 #[cfg(unix)]
 pub const FSLATEAPPLICATION_ONRAWMOUSEMOVE: usize = 0x1ce9c70;
 #[cfg(windows)]
-pub const FSLATEAPPLICATION_ONRAWMOUSEMOVE: usize = 0x322610;
+pub const FSLATEAPPLICATION_ONRAWMOUSEMOVE: usize = 0x32a540;
 
 // ACharacter::CheckJumpInput(ACharacter* this, float)
 // _ZN10ACharacter14CheckJumpInputEf
@@ -54,4 +54,4 @@ pub const FSLATEAPPLICATION_ONRAWMOUSEMOVE: usize = 0x322610;
 #[cfg(unix)]
 pub const ACONTROLLER_GETCONTROLROTATION: usize = 0x2b151e0;
 #[cfg(windows)]
-pub const ACONTROLLER_GETCONTROLROTATION: usize = 0xb9ef40;
+pub const ACONTROLLER_GETCONTROLROTATION: usize = 0xba1120;
