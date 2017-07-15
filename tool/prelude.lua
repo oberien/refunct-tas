c2cf91f887531fec3a737dff7550b063b285fb61
oberien <jaro.fietz@gmx.de>
2017-07-15 23:15:00 +0200

    Finish teleportbuttons

--- tool/prelude.lua
+++ tool/prelude.lua
@@ -151,3 +151,10 @@ function frame(keys, degx, degy, repeatnum)
     execframe(currentframe)
   end
 end
+
+
+function printstats()
+  stats = getplayerstats()
+  print(string.format("x: %-6.2f\ty: %-6.2f\tz: %-6.2f\tvelx: %-6.2f\tvely: %-6.2f\tvelz: %-6.2f\tpitch: %-6.2f\tyaw: %-6.2f\taccx: %-6.2f\taccy: %-6.2f\t",
+		stats.x, stats.y, stats.z, stats.velx, stats.vely, stats.velz, stats.pitch, stats.yaw, stats.accx, stats.accy))
+end
