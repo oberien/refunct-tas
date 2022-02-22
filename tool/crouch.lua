43d43135c374cac873341a3f162a3e9454d1256f
oberien <jaro.fietz@gmx.de>
2022-02-22 23:18:25 +0100

    Remove old lua files

--- tool/crouch.lua
+++
@@ -1,6 +0,0 @@
-require "prelude"
-
-frame({}, 0, 0, 250)
-frame({forward}, 0, 0, 10)
-frame({forward, crouch}, 0, 0, 60)
-frame()
--- tool/printstats.lua
+++
@@ -1,6 +0,0 @@
-require "prelude"
-
-while true do
-  printstats()
-  step()
-end
