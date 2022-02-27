d7b3efd1d0612d8ccbf6a6b1c4fb1b81497e4b52
oberien <jaro.fietz@gmx.de>
2022-02-27 16:46:15 +0100

    Update dependencies of dynsym

--- dynsym/src/lib.rs
+++ dynsym/src/lib.rs
@@ -5,22 +5,21 @@ extern crate memmap;
 use std::path::Path;
 use std::fs::File;
 
-use object::{ElfFile, Object};
+use object::{Object, ObjectSymbol};
 use cpp_demangle::{Symbol, DemangleOptions};
 use memmap::Mmap;
 
 pub fn iter<P: AsRef<Path>>(path: P) -> Vec<(String, usize)> {
     let file = File::open(path).unwrap();
     let file = unsafe { Mmap::map(&file) }.unwrap();
-    let file = ElfFile::parse(&*file).unwrap();
-    file.dynamic_symbols().into_iter()
-        .flat_map(|sym| sym.name()
+    let file = object::File::parse(&*file).unwrap();
+    file.dynamic_symbols()
+        .flat_map(|sym| sym.name_bytes().ok()
             .and_then(|name| Symbol::new(name).ok())
             .and_then(move |symbol| {
-                let options = DemangleOptions { no_params: false };
-                symbol.demangle(&options).ok()
+                symbol.demangle(&DemangleOptions::new()).ok()
             })
-            .or_else(|| sym.name().map(|s| s.to_string()))
+            .or_else(|| sym.name().map(|s| s.to_string()).ok())
             .map(|name| (name, sym.address() as usize)))
         .collect()
 }
