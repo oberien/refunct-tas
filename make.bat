d944acc33f95b7a162bf6bde984ea8912c26832b
oberien <jaro.fietz@gmx.de>
2022-03-23 22:22:28 +0000

    add settings.re to make.bat

--- make.bat
+++ make.bat
@@ -21,6 +21,7 @@ copy tool\randomizer.re build\practice-windows
 copy tool\teleport.re build\practice-windows
 copy tool\ui.re build\practice-windows
 copy tool\multiplayer.re build\practice-windows
+copy tool\settings.re build\practice-windows
 echo Converting lf to crlf
 call :convert main.re
 call :convert component.re
@@ -31,6 +32,7 @@ call :convert randomizer.re
 call :convert teleport.re
 call :convert ui.re
 call :convert multiplayer.re
+call :convert settings.re
 
 echo Don't forget to create a zip
 exit /b 0
