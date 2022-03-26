489e03d92a7f51f7a11bc7f46a98d2d05ca48145
oberien <jaro.fietz@gmx.de>
2022-03-26 13:54:00 +0100

    Receive messages in server-test

--- server-test/src/main.rs
+++ server-test/src/main.rs
@@ -6,15 +6,27 @@ use websocket::{ClientBuilder, Message, OwnedMessage, WebSocketError};
 use protocol::Request;
 
 fn main() {
+    let name = std::env::args().nth(1).unwrap_or_else(|| "Test".to_string());
     // let mut client = ClientBuilder::new("ws://localhost:8080/ws").unwrap().connect(None).unwrap();
     let mut client = ClientBuilder::new("wss://refunct-tas.oberien.de/ws").unwrap().connect(None).unwrap();
-    let msg = Request::JoinRoom("Test".to_string(), "Test".to_string(), 0.0, 0.0, 0.0);
+    let msg = Request::JoinRoom("Test".to_string(), name, 0.0, 0.0, 0.0);
     client.send_message(&Message::text(serde_json::to_string(&msg).unwrap())).unwrap();
     thread::sleep(Duration::new(5, 0));
     for &(x, y, z) in LOCATIONS.iter() {
-        thread::sleep(Duration::new(0, (1_000_000_000.0 / 60.0) as u32));
+        thread::sleep(Duration::new(0, (1_000_000_000 / 60) as u32));
         let msg = Request::MoveSelf(x, y, z);
         client.send_message(&Message::text(serde_json::to_string(&msg).unwrap())).unwrap();
+        loop {
+            client.set_nonblocking(true).unwrap();
+            let res = client.recv_message();
+            client.set_nonblocking(false).unwrap();
+            match res {
+                Ok(OwnedMessage::Text(_)) => (),
+                Ok(OwnedMessage::Binary(_) | OwnedMessage::Ping(_) | OwnedMessage::Pong(_)) => continue,
+                Err(WebSocketError::IoError(io)) if io.kind() == ErrorKind::WouldBlock => break,
+                Ok(OwnedMessage::Close(_)) | Err(_) => break,
+            }
+        }
     }
 }
 
