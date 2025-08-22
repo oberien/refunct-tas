d685a71b8cbafa38b943e268044f16dc85a6f940
Luke Saward <luke.sawardmgs@gmail.com>
2025-08-22 23:28:10 +0100

    fix typo

--- gen-consts/src/main.rs
+++ gen-consts/src/main.rs
@@ -57,7 +57,7 @@ const NAMES: &[(&str, &str)] = &[
     ("?SetGameRenderingEnabled@FViewport@@SAX_NH@Z","FVIEWPORT_SETGAMERENDERINGENABLED"),
     ("?SetInputMode_GameOnly@UWidgetBlueprintLibrary@@SAXPAVAPlayerController@@@Z", "UWIDGETBLUEPRINTLIBRARY_SETINPUTMODE_GAMEONLY"),
     ("?SetInputMode_UIOnlyEx@UWidgetBlueprintLibrary@@SAXPAVAPlayerController@@PAVUWidget@@W4EMouseLockMode@@@Z", "UWIDGETBLUEPRINTLIBRARY_SETINPUTMODE_UIONLYEX"),
-    ("??FlushPressedKeys@APlayerController@@UAEXXZ", "APLAYERCONTROLLER_FLUSHPRESSEDKEYS"),
+    ("?FlushPressedKeys@APlayerController@@UAEXXZ", "APLAYERCONTROLLER_FLUSHPRESSEDKEYS"),
 ];
 
 fn get_linux_level_pointer_path() -> String {
